fa2b49bd00b9696b7c19695abc581c0d87e3d3ff
Qudus Qazeem <qudusqazeem@gmail.com>
2023-08-07 06:47:31 +0100

    Initial Commit

---
+++ pulavestios/pulavestiosApp.swift
@@ -0,0 +1,17 @@
+//
+//  pulavestiosApp.swift
+//  pulavestios
+//
+//  Created by Qazeem Abiodun on 07/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct pulavestiosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
