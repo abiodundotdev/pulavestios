c474333e63a79fb68db431581e12f92070dbcc25
Qazeem Abiodun <qudusqazeem@gmail.com>
2023-10-21 20:25:16 +0100

    update ui

--- pulavestios/Presentation/Auth/LoginPage.swift
+++ pulavestios/Presentation/Auth/LoginPage.swift
@@ -66,7 +66,6 @@ struct LoginPage: View {
 struct LoginPage_Previews: PreviewProvider {
     static var previews: some View {
         LoginPage().environmentObject(ThemeManager())
-        //spage
     }
 }
 
--- pulavestios/Presentation/DashboardPage.swift
+++ pulavestios/Presentation/DashboardPage.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+import SwiftUI
+
+struct DashboardPage : View {
+    var body : some View {
+        VStack(spacing: 50) {
+            PhaseAnimator([0, 1, 3]) { value in
+                Text("Hello, world!")
+                    .font(.largeTitle)
+                    .scaleEffect(value)
+                    .opacity(value == 1 ? 1 : 0)
+                
+                Text("Goodbye, world!")
+                    .font(.largeTitle)
+                    .scaleEffect(3 - value)
+                    .opacity(value == 1 ? 1 : 0)
+            }
+        }
+    }
+}
+
+struct DashboardPagepreview: PreviewProvider{
+    static var previews: some View {
+        DashboardPage()
+    }
+}
